22ec50e263898ed293c812ec757c2a0ac03ac33a
AnaG0307 <ana.genover@gmail.com>
2021-07-21 18:29:23 +0000

    Create update_surplus_worksheet to update excel with the surplus calculated with the data entered by user

--- run.py
+++ run.py
@@ -84,6 +84,16 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def update_surplus_worksheet(calculate_surplus_data):
+    """
+    Update surplus worksheet, add new row with surplus calculated.
+    """
+    print("Updating surplus worksheet...")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(calculate_surplus_data)
+    print("Surplus worksheet succesfully updated\n")
+
+
 def main():
     """
     Run all program functions
@@ -93,6 +103,7 @@ def main():
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automotion")
 main()
