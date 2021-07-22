fab59079534d1d729f968d74c9001353e39aa16c
AnaG0307 <ana.genover@gmail.com>
2021-07-22 09:24:57 +0000

    Collect sales data for the last 5 das in lists of columns

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
 
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -106,6 +107,22 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_the_last_5_sales():
+    """
+    Collects columns of data from sales worksheet, colelcting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet('sales')
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
+
+
 def main():
     """
     Run all program functions
@@ -117,4 +134,6 @@ def main():
     update_worksheet(new_surplus_data, 'surplus')
 
 print("Welcome to Love Sandwiches Data Automotion")
+
 main()
+sales_columns = get_the_last_5_sales()
